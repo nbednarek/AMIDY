18697e051219f790f534cf072776afd16f0f2b62
nbednarek <144115800+nbednarek@users.noreply.github.com>
2024-01-20 13:26:50 +0100

    Delete APIWEATHER directory

--- APIWEATHER/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.apiweather {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires json.simple;
-
-
-    opens com.example.apiweather to javafx.fxml;
-    exports com.example.apiweather;
-}
